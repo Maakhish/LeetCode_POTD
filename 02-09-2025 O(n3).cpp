d2d85b1d5732ee52d2c7a0574a7b9e0b715008d8
Maahi <153072634+Maakhish@users.noreply.github.com>
2025-09-02 18:30:35 +0530

    Create 02-09-2025 O(n3).cpp

---
+++ 02-09-2025 O(n3).cpp
@@ -0,0 +1,39 @@
+class Solution {
+private: 
+    bool luhs(vector<int>& a, vector<int>& b){
+        return (a[0] <= b[0]) && (a[1] >= b[1]);
+    }
+    bool noOtherInside(vector<int>& a, vector<int>& b, vector<vector<int>>& points){
+        for(auto p: points){
+            int x = p[0];
+            int y = p[1];
+            if((x == a[0] && y == a[1]) || (x == b[0] && y == b[1]))
+                continue;
+            if((a[0] <= x && x <= b[0]) && (a[1] >= y && y >= b[1]))
+                return false; 
+        }
+        return true;
+    }
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int n = points.size();
+        sort(points.begin(), points.end());
+        int ans = 0;
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j < n; j++){
+                auto p1 = points[i];
+                auto p2 = points[j];
+                if(luhs(p1, p2)){
+                    if(noOtherInside(p1, p2, points)){
+                        ans++;
+                    }
+                }
+                if(luhs(p2, p1)){
+                    if(noOtherInside(p2, p1, points))
+                        ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
